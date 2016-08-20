4c4a4712e70b3868a1fc5115f08e5f9042421f00
John Weaver <saebynx+github@gmail.com>
2016-08-19 17:14:34 -0700

    Update endpoints to match 2015-03-31 version of Lambda API

--- web.js
+++ web.js
@@ -19,23 +19,40 @@ function LambdaRESTAPI(locavore) {
 			});
 		});
 	});
+	
+	app.post('/2015-03-31/functions/:fn/invocations', function(req, res) { // Invoke
+		locavore.invoke(req.params.fn, req.body, function(err, id) {
+			if ( err ) {
+				res.status(404)
+			} else {
+				res.header('x-amzn-requestid', id);
+			}
+			
+		}, function (err, result) {
+			if (err) {
+				res.status(500)
+			}
+			
+			res.send(result)
+		});
+	});
 
-	app.get('/2014-11-13/functions/', function(req, res) { // ListFunctions
+	app.get('/2015-03-31/functions/', function(req, res) { // ListFunctions
 		locavore.functionList(function(err, list) {
 			if (err) res.send(500);
 			else res.send({ Functions: list });
 		});
 	});
 
-	app.post('/2014-11-13/event-source-mappings/', notImplemented); // AddEventSource
-	app.delete('/2014-11-13/functions/:fn', notImplemented); // DeleteFunction
-	app.get('/2014-11-13/event-source-mappings/:uuid', notImplemented); // GetEventSource
-	app.get('/2014-11-13/functions/:fn', notImplemented); // GetFunction
-	app.get('/2014-11-13/functions/:fn/configuration', notImplemented); // GetFunctionConfiguration
-	app.get('/2014-11-13/event-source-mappings/', notImplemented); // ListEventSources
-	app.delete('/2014-11-13/event-source-mappings/:uuid', notImplemented); // RemoveEventSource
-	app.put('/2014-11-13/functions/:fn/configuration', notImplemented); // UpdateFunctionConfiguration
-	app.put('/2014-11-13/functions/:fn', notImplemented); // UploadFunction
+	app.post('/2015-03-31/event-source-mappings/', notImplemented); // AddEventSource
+	app.delete('/2015-03-31/functions/:fn', notImplemented); // DeleteFunction
+	app.get('/2015-03-31/event-source-mappings/:uuid', notImplemented); // GetEventSource
+	app.get('/2015-03-31/functions/:fn', notImplemented); // GetFunction
+	app.get('/2015-03-31/functions/:fn/configuration', notImplemented); // GetFunctionConfiguration
+	app.get('/2015-03-31/event-source-mappings/', notImplemented); // ListEventSources
+	app.delete('/2015-03-31/event-source-mappings/:uuid', notImplemented); // RemoveEventSource
+	app.put('/2015-03-31/functions/:fn/configuration', notImplemented); // UpdateFunctionConfiguration
+	app.put('/2015-03-31/functions/:fn', notImplemented); // UploadFunction
 
 }
 
